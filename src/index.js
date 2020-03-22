2734f482b58e179669fb4f1cf5fda3dc0602bbd2
Novikov Stanislav <novikov.stan@mail.ru>
2020-03-22 19:30:50 +0300

    feat typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length != 0) {
+        array.sort((a,b) => {
+            if (a > b) return 1;
+            else if (a < b ) return -1;
+            else return 0;
+        })
+        return array.shift();
+    } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length != 0 ) {
+        array.sort((a,b) => {
+            if (a > b) return 1;
+            else if (a < b ) return -1;
+            else return 0;
+        })
+        return array.pop();
+    } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length != 0) {
+        return array.reduce((prev, current) => current += prev,0)/array.length;
+
+    } else return 0;
 }
